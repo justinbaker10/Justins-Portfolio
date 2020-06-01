1984c5cb89390f7e805cf97f2427fc2663f50593
justinbaker10 <justin-baker@live.com>
2020-06-01 11:16:31 -0500

    created landing page, navbar, and some graphics

---
+++ app.js
@@ -0,0 +1,11 @@
+// const express = require('express')
+// //creates express app
+// const app = express()
+// //sets the path to the html file
+// const path = require('path')
+// //a sample route
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join('index.html' { root: portfolio-site }));
+// });
+// // Start the Express server
+// app.listen(3000, () => console.log('Server running on port 3000!'))
